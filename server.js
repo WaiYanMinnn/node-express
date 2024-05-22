ea9977db6eb91f5367e265ef3c88073ad2568ec6
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-05-21 19:05:01 -0700

    Express server created

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+
+const hostname='localhost';
+const port = 3000;
+
+const app =express();
+
+app.use((req,res)=>{
+    console.log(req.header);
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<html><body><h1>This is an express server</h1></body></html>');
+
+});
+app.listen(port,hostname,()=>{
+    console.log(`Server Running at http://${hostname}:${port}`);
+
+})
