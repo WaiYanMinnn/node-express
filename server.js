cc1dba180968472ac2fdb6d9a9f2c1cf6d5efc4a
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-05-21 21:51:50 -0700

    express router

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const morgan=require('morgan');
+const campsiteRouter=require('./routes/campsiteRouter');
 
 const hostname='localhost';
 const port = 3000;
@@ -9,49 +10,7 @@ const app =express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.all('/campsites',(req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
-
-app.get('/campsites',(req,res)=>{
-    res.end('Will send all the campsites to you');
-});
-app.post('/campsites',(req,res)=>{
-    res.end(`Will add the campsite:${req.body.name} with the description of ${req.body.description}`);
-});
-
-app.put('/campsites',(req,res)=>{
-    res.statusCode=403;
-    res.end('PUT operation is not supported on /campsites');
-
-});
-
-app.delete('/campsites',(req,res)=>{
-    res.end('Deleting all campsites');
-
-});
-
-app.get('/campsites/:campsiteId',(req,res)=>{
-    res.end(`Will send detail of the campsite : ${req.params.campsiteId} to you`);
-
-});
-
-app.post ('/campsites/:campsiteId',(req,res)=>{
-    res.statusCode=403;
-    res.end(`POST operation is not supported on /campsite/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId',(req,res)=>{
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end (`Will update the campsite: ${req.body.name} with the description: ${req.body.description}`);
-
-});
-app.delete('/campsites/:campsiteId',(req,res)=>{
-    res.end(`Deleting the campsite : ${req.params.campsiteId}`);
-});
-
+app.use('/campsites',campsiteRouter);
 
 
 app.use(express.static(__dirname+ '/public'));
